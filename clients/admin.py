5d5ef26c731392bfca7e6a674bcd6696df9ed8ce
Lindsaykerr1994 <lindsaykerr1994@gmail.com>
2021-02-09 18:26:35 +0000

    Updated admin for client

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Client
 
-# Register your models here.
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'email_address',
+        'phone_number'
+    )
+
+    ordering = ('last_name', 'first_name')
+
+
+admin.site.register(Client, ClientAdmin)
--- clients/models.py
+++ clients/models.py
@@ -9,7 +9,6 @@ class Client(models.Model):
     street_address1 = models.CharField(max_length=254, null=True, blank=True)
     street_address2 = models.CharField(max_length=254, null=True, blank=True)
     postcode = models.CharField(max_length=10, null=True, blank=True)
-    
 
     def __str__(self):
         return (self.last_name + ", " + self.first_name)
