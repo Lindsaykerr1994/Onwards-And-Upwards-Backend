db25b4bb8af08d1364a6d36250b0fb72724a3549
Lindsaykerr1994 <lindsaykerr1994@gmail.com>
2021-02-10 16:48:07 +0000

    Added client forms.py and created template view_client.html

--- clients/models.py
+++ clients/models.py
@@ -9,6 +9,8 @@ class Client(models.Model):
     street_address1 = models.CharField(max_length=254, null=True, blank=True)
     street_address2 = models.CharField(max_length=254, null=True, blank=True)
     postcode = models.CharField(max_length=10, null=True, blank=True)
+    additional_info = models.TextField(null=True, blank=True)
+    root_of_inquiry = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return (self.last_name + ", " + self.first_name)
--- clients/views.py
+++ clients/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from activities.models import Activity, Course
 from .models import Client
 
@@ -10,8 +11,18 @@ def all_clients(request):
     }
     return render(request, 'clients/all_clients.html', context)
 
-
+@login_required
 def add_client(request):
+    if request.GET:
+        print("Add client functionality")
     context = {
     }
     return render(request, 'clients/add_client.html', context)
+
+
+def view_client(request, client_id):
+    client = get_object_or_404(Client, pk=client_id)
+    context = {
+        'client': client
+    }
+    return render(request, 'clients/view_client.html', context)
