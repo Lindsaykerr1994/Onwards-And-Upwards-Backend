1bd5d52c1b3afdf8f585f40279fa1215c923af5e
Lindsaykerr1994 <lindsaykerr1994@gmail.com>
2021-02-11 15:28:44 +0000

    Added field to client model and built edit client form

--- clients/models.py
+++ clients/models.py
@@ -8,6 +8,7 @@ class Client(models.Model):
     phone_number = models.CharField(max_length=15, null=True, blank=True)
     street_address1 = models.CharField(max_length=254, null=True, blank=True)
     street_address2 = models.CharField(max_length=254, null=True, blank=True)
+    town_or_city = models.CharField(max_length=254, null=True, blank=True)
     postcode = models.CharField(max_length=10, null=True, blank=True)
     additional_info = models.TextField(null=True, blank=True)
     root_of_inquiry = models.CharField(max_length=254, null=False, blank=False,
