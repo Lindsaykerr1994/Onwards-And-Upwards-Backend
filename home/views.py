31bca9b5c12f10a84becc56bfcf8e7ec66d759f4
Lindsaykerr1994 <lindsaykerr1994@gmail.com>
2021-03-18 16:07:13 +0000

    Updated welcome page to display user name

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from appointments.models import Appointment
+from django.contrib.auth.models import User
 
 
 @login_required
@@ -9,8 +10,10 @@ def index(request):
     if not request.user.is_staff:
         messages.error(request, "Sorry, You don't have permission to do that.")
         return redirect(reverse('home'))
+    profile = get_object_or_404(User, id=request.user.id)
     appointments = Appointment.objects.all()
     context = {
+        'profile': profile,
         'appointments': appointments
     }
     return render(request, 'home/index.html', context)
