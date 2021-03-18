c7c37e85e2c3171d3e8c74f09db5acc88adb6811
Lindsaykerr1994 <lindsaykerr1994@gmail.com>
2021-03-18 13:13:07 +0000

    Removed secret key and set debug

--- onwards_backend/settings.py
+++ onwards_backend/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!)p#3uvqu+e)wc@xae-)bys4u5vgw5*ir-u(@l@pn1n($^n$pp'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['onwards-and-upwards.herokuapp.com', 'localhost']
 
