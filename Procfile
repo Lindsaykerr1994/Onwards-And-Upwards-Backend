e90919664b89e5928db4bc1c1e6f6aae8d1df95d
Lindsaykerr1994 <lindsaykerr1994@gmail.com>
2021-03-18 12:51:19 +0000

    Deploying part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn onwards_backend.wsgi:application
--- onwards_backend/settings.py
+++ onwards_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '!)p#3uvqu+e)wc@xae-)bys4u5vgw5*ir-u(@l@pn1n($^n$pp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['onwards-and-upwards.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -107,12 +107,17 @@ WSGI_APPLICATION = 'onwards_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.envrion:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
