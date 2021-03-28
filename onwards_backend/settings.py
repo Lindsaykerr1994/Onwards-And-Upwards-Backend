63761f3d359405ba1a12e913d05422d06b363e86
Lindsaykerr1994 <lindsaykerr1994@gmail.com>
2021-03-28 17:41:50 +0000

    Changed port for email in settings.py

--- onwards_backend/settings.py
+++ onwards_backend/settings.py
@@ -198,7 +198,7 @@ if 'DEVELOPMENT' in os.environ:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_SSL = True
-    EMAIL_PORT = 587
+    EMAIL_PORT = 465
     EMAIL_HOST = 'mail.onwards-and-upwards.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
